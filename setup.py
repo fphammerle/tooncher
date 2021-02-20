4644d19eaf3798c97d2cb5a020baff31abff58e3
Fabian Peter Hammerle <fabian@hammerle.me>
2021-02-20 10:11:35 +0100

    test against python3.9
    https://github.com/fphammerle/switchbot-mqtt/commit/b2747c3a3a5251285732694945fa836ee3552600
    https://github.com/fphammerle/ical2vdir/commit/d679a828a6de976c477ddfa7f3ced0b1be57fc3c

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Games/Entertainment",
         "Topic :: Utilities",
     ],
